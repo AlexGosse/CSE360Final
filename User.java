849ce7b135c906f520e323c9287dfcff58112083
Gangweed <44474940+Gangweed@users.noreply.github.com>
2020-11-21 17:17:01 -0700

    User class with user info

---
+++ User.java
@@ -0,0 +1,16 @@
+public class User {
+    private int ID;
+    private String firstName, lastName, ProgramAndPlan, AcademicLevel, ASURITE;
+
+    public User(int ID, String firstName, String lastName, String ProgramAndPlan, String AcademicLevel, String ASURITE){
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ProgramAndPlan = ProgramAndPlan;
+        this.AcademicLevel = AcademicLevel;
+        this.ASURITE = ASURITE;
+    }
+
+    //Getters and setters
+
+}
